d5c8cdb6b232a9ce810c37432569b0a16d7ed01d
hoang <vinhyenvodoi98@gmail.com>
2019-12-10 16:59:09 +0700

    feat : deploy github page

---
+++ frontend/build/precache-manifest.3684d760e44f613bcb1e0e74bca28b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effe4dad8d9fc321774a214ff9bc5162",
+    "url": "/Raspberry-Reactjs-plant-tracking/index.html"
+  },
+  {
+    "revision": "f70fc9522158285eaea2",
+    "url": "/Raspberry-Reactjs-plant-tracking/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "0e8c13fa24db52bfa67a",
+    "url": "/Raspberry-Reactjs-plant-tracking/static/css/main.55d79715.chunk.css"
+  },
+  {
+    "revision": "f70fc9522158285eaea2",
+    "url": "/Raspberry-Reactjs-plant-tracking/static/js/2.a8a4a612.chunk.js"
+  },
+  {
+    "revision": "0e8c13fa24db52bfa67a",
+    "url": "/Raspberry-Reactjs-plant-tracking/static/js/main.a52c52c4.chunk.js"
+  },
+  {
+    "revision": "f98121c34f79ecadea0f",
+    "url": "/Raspberry-Reactjs-plant-tracking/static/js/runtime-main.60179c3c.js"
+  },
+  {
+    "revision": "34b33eddaefbae82b623e599659740ab",
+    "url": "/Raspberry-Reactjs-plant-tracking/static/media/Modernica.34b33edd.jpg"
+  }
+]);
